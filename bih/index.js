05ec3584b970f1d1a445989265b2090c8068de06
Jon-Carlos Rivera <imbcmdth@hotmail.com>
2012-07-27 12:22:55 -0400

    Added tree.each and tree.intersectStep functions

--- bih/index.js
+++ bih/index.js
@@ -230,6 +230,296 @@ BIH.prototype = {
 		});
 	},
 
+	// tree.each (recursive)
+	// go over each node and leaf in the tree (depth-first) and execute the provided
+	// callback functions.
+	// Very useful for drawing the tree, counting nodes, ect.
+	each : function(nodeCallback, leafCallback) {
+		function iterate(node, currentAABB, depth){
+			var axis,
+			    leftAABB,
+			    rightAABB, retVal;
+
+			if(node.x){
+				axis = node.x - 1;
+
+				leftAABB = currentAABB.clone();
+				leftAABB.max[axis] = node.u;
+
+				rightAABB = currentAABB.clone();
+				rightAABB.min[axis] = node.v;
+
+				if(nodeCallback) retVal = nodeCallback(node, leftAABB, rightAABB, depth);
+				if(!retVal) return;
+
+				iterate(node.l, leftAABB, depth+1);
+				iterate(node.r, rightAABB, depth+1);
+			} else if(node.o) {
+				if(leafCallback) leafCallback(node, currentAABB, depth);
+			}
+		}
+
+		iterate(this._T, this.i, 0);
+	},
+
+	// tree.intersectStep 
+	// intersect the tree with ray returning the earliest element that hits the ray
+	// returns information to a callback and provides a next function to continue 
+	// traversal
+	//
+	// Based on Havran's TA-B -
+	// Modified for BIH with several new cases:
+	//  - I1 (ray enters between planes, exits in left node)
+	//  - I2 (ray enters between planes, exits in right node)
+	//  - I3 (ray enters between planes, exits between planes)
+	//  - B1 (Ray enters in both planes, exits both)
+	//  - B2 (Ray enters in both planes, exits left)
+	//  - B3 (Ray enters in both planes, exits right)
+	//
+	// Modified for incremental(on-demand?) tree building
+	//
+	// stepCallback(node, rs, currentAABB, wasLeft, depth, next)
+	intersectStep : function(ray, intersectInfo, stepCallback, finishedCallback) {
+		var parentStack = [], // Contains the nodes that are parents of the hit nodes
+		    rayStack = [], // Contains the ray-segment for the current sub-tree
+		    depthStack = [], // Just for depth determining. debugging stuff.
+		    directionStack = [], // Which was the last direction taken - true = left
+		    aabbStack = [],
+		    currentAABB = this.i.clone(),
+		    tempAABB,
+		    lastDirectionWasLeft = true,
+		    rayIntervals = ray.toIntervals(),
+		    majorAxis = ray.getMajorAxis(),
+		    node = null,
+		    parentNode = null,
+		    rs = null,
+		    axis = null,
+		    newRS = null,
+		    bestSegment = this.segmentHelpers.cloneSegment(rayIntervals),
+		    leafElementCount,
+		    leafElement = null,
+		    noDebug = true,
+		    debug = null,
+		    thisTree = this;
+
+		if(intersectInfo.debug) {
+			noDebug = false;
+			debug = intersectInfo.debug;
+			depthStack = [];
+		}
+
+		// Test ray AABB first
+		rs = this.i.intersectWithSegment(rayIntervals);
+
+		// If there are no elements or the ray-AABB test failed, don't bother traversing
+		if (rs && this._T !== null) {
+			node = this._T;
+		} else {
+			return stepCallback(null, rs, currentAABB, null, 0, null);
+		}
+
+		stepCallback(node, rs, currentAABB, null, 0, step);
+
+		function step() {
+			while( (node !== null) || parentStack.length > 0) {
+				if(!noDebug) debug.currDepth++;
+
+				if(node === null) {
+					rs = rayStack.pop(); // Depth-First Descent
+					parentNode = parentStack.pop();
+					lastDirectionWasLeft = directionStack.pop();
+					currentAABB = aabbStack.pop();
+					if(!noDebug) {
+						debug.depth = Math.max(debug.depth, debug.currDepth);
+						debug.currDepth = depthStack.pop();
+					}
+
+
+					if(lastDirectionWasLeft)
+						node = parentNode.l;
+					else
+						node = parentNode.r;
+				}
+
+				// Check to see if this node is still reachable
+				if(intersectInfo.isHit) {
+					if(!thisTree.segmentHelpers.trimSegmentInPlace(bestSegment, rs, majorAxis)) {
+						node = null;
+						continue;
+					}
+				}
+
+				if(!noDebug) debug.costT++;
+
+				if(node.s) { // An unfinished node!
+					node = thisTree._incrementalBuild(node);
+					if(parentNode == null) {
+						thisTree._T = node;
+					} else {
+						if(lastDirectionWasLeft)
+							parentNode.l = node;
+						else
+							parentNode.r = node;
+					}
+				}
+
+				// Below is not an "else if" because we want to continue traversal during an incremental build
+				if (node.x) { // A node!
+					axis = node.x - 1; // Axis: 1 = x, 2 = y, 3 = z ...
+					// Cases where entry point is between *both* splitting planes
+					if (rs[axis].a < node.v && rs[axis].a > node.u) {
+						if (rs[axis].b <= node.u) { // Is the exit point inside the left node?
+							/* case I1 */
+							// The ray enters the volume between the planes so
+							// we need to clip the ray start for this case
+							thisTree.segmentHelpers.clipSegmentStartInPlace(rs, axis, node.u);
+							parentNode = node;
+							lastDirectionWasLeft = true;
+							currentAABB.max[axis] = node.u;
+							node = node.l;
+						} else if (rs[axis].b >= node.v) { // Is the exit point inside the right node?
+							/* case I2 */
+							// The ray enters the volume between the planes so
+							// we need to clip the ray start for this case
+							thisTree.segmentHelpers.clipSegmentStartInPlace(rs, axis, node.v);
+							parentNode = node;
+							lastDirectionWasLeft = false;
+							currentAABB.min[axis] = node.v;
+							node = node.r;
+						} // If start is between both planes,
+						// the end point CAN NOT be in BOTH nodes - it is unpossible
+						 else {
+							node = null;
+						}
+					} else if (rs[axis].a <= node.u) { // Starts in left node
+						if (rs[axis].a >= node.v) { // Also in right node!
+							// If we exit and are no longer in the right node, we must clip the ray
+							if (rs[axis].b < node.v) {
+								newRS = thisTree.segmentHelpers.clipSegmentEnd(rs, axis, node.v);
+							} else {
+								newRS = thisTree.segmentHelpers.cloneSegment(rs);
+							}
+							// This will be popped later, so right = far node
+							rayStack.push(newRS);
+							parentStack.push(node);
+							directionStack.push(false);
+							tempAABB = currentAABB.clone();
+							tempAABB.min[axis] = node.v;
+							aabbStack.push(tempAABB);
+							if(!noDebug) depthStack.push(debug.currDepth);
+							
+							// If we exit and are no longer in the left node, we must clip the ray
+							if (rs[axis].b > node.u) {
+								thisTree.segmentHelpers.clipSegmentEndInPlace(rs, axis, node.u);
+							}
+							// This will be popped first, so left = near node
+							parentNode = node;
+							lastDirectionWasLeft = true;
+							currentAABB.max[axis] = node.u;
+							node = node.l;
+						} else if (rs[axis].b < node.v) { // We are exiting before the right plane
+							if (rs[axis].b <= node.u) {
+								// We are exiting before the left plane
+								/* cases N1,N2,N3,P5,Z2,Z3 */
+								parentNode = node;
+								lastDirectionWasLeft = true;
+								currentAABB.max[axis] = node.u;
+								node = node.l;
+							} else {
+								// The ray exits the volume between the planes so
+								// we need to clip the ray end for this case
+								thisTree.segmentHelpers.clipSegmentEndInPlace(rs, axis, node.u);
+								parentNode = node;
+								lastDirectionWasLeft = true;
+								currentAABB.max[axis] = node.u;
+								node = node.l;
+							}
+						} else { // The ray exits on the far side of the right plane
+							/* case N4 */
+							// This will be popped later, so right = far node
+							newRS = thisTree.segmentHelpers.clipSegmentStart(rs, axis, node.v);
+
+							rayStack.push(newRS);
+							parentStack.push(node);
+							directionStack.push(false);
+							tempAABB = currentAABB.clone();
+							tempAABB.min[axis] = node.v;
+							aabbStack.push(tempAABB);
+							if(!noDebug) depthStack.push(debug.currDepth);
+
+							// This will be popped first, so left = near node
+							thisTree.segmentHelpers.clipSegmentEndInPlace(rs, axis, node.u);
+							parentNode = node;
+							lastDirectionWasLeft = true;
+							currentAABB.max[axis] = node.u;
+							node = node.l;
+						}
+					} else if (rs[axis].a >= node.v) { // Starts in right node
+						if (rs[axis].b > node.u) { // Ray exits before the left plane
+							if (rs[axis].b >= node.v) { // Ray exits before the right plane
+								/* cases P1,P2,P3,N5,Z1 */
+								parentNode = node;
+								lastDirectionWasLeft = false;
+								currentAABB.min[axis] = node.v;
+								node = node.r;
+							} else { /* cases P1,P2,P3,N5,Z1 */
+								// we need to clip the ray end for this case
+								thisTree.segmentHelpers.clipSegmentEndInPlace(rs, axis, node.v);
+								parentNode = node;
+								lastDirectionWasLeft = false;
+								currentAABB.min[axis] = node.v;
+								node = node.r;
+							}
+						} else { // Ray hits both planes
+							/* case P4 */
+							// This will be popped later, so left = far node
+							newRS = thisTree.segmentHelpers.clipSegmentStart(rs, axis, node.u);
+							rayStack.push(newRS);
+							parentStack.push(node);
+							directionStack.push(true);
+							tempAABB = currentAABB.clone();
+							tempAABB.max[axis] = node.u;
+							aabbStack.push(tempAABB);
+							if(!noDebug) depthStack.push(debug.currDepth);
+
+							// This will be popped first, so right = near node
+							thisTree.segmentHelpers.clipSegmentEndInPlace(rs, axis, node.v);
+							parentNode = node;
+							lastDirectionWasLeft = false;
+							currentAABB.min[axis] = node.v;
+							node = node.r;
+						}
+					} else {
+						node = null;
+					}
+				}
+
+				if (node && node.x) return stepCallback(node, rs, currentAABB, lastDirectionWasLeft, debug.currDepth, step);
+
+				if (node && node.o) { // A leaf!!
+					leafElementCount = node.o.length;
+					while(leafElementCount-->0) {
+						if(!noDebug) debug.costI++;
+
+						leafElement = node.o[leafElementCount];
+						leafElement.iFn.call(leafElement.o, ray, intersectInfo);
+
+						if(intersectInfo.isHit) {
+							thisTree.segmentHelpers.setBestSegment(bestSegment, intersectInfo.position);
+						}
+					}// end while each element
+
+					// Causes the loop to pop far-node candidates from the stack
+					stepCallback(node, rs, currentAABB, lastDirectionWasLeft, debug.currDepth, step);
+					node = null;
+					return;
+				}
+			}// end of main while()
+			if(!noDebug) debug.depth = Math.max(debug.depth, debug.currDepth);
+			if(finishedCallback) finishedCallback(intersectInfo);
+		}
+	},
+
 	// tree.intersect (non-recursive)
 	// intersect the tree with ray returning the earliest element that hits the ray
 	//
